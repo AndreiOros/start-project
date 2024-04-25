9f2e835c4145b9192f3a2770d2cd14680ee9e138
Andrei <andrei.oros111@gmail.com>
2024-04-25 10:47:31 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <p>Hello World!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              esModule: false,
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
